70166c3636d710db625b62f4bc3a6db9780ec15c
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-11-08 15:55:01 +0530

    Update VotersList.js

--- src/components/VotersList.js
+++ src/components/VotersList.js
@@ -25,6 +25,7 @@ function VotersList(){
         <div>
             <AdminNav/>
             <Link to={"/Admin/AddVoter/"+id}><button class="mx-5 mt-5 btn " style={{backgroundColor:"rgb(43,24,155)",color:"white"}}>Add A Voter</button></Link>
+    <div style{{overflowX:"auto"}}>
             <table  class="container table table-bordered mt-3">
             <thead class="table-info ">
                 <tr>
@@ -43,6 +44,7 @@ function VotersList(){
             </tbody>
             
         </table>
+    </div>
         </div>
         
     )
