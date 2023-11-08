3e5cf8a31f03acaea649b53534d8019f6e465995
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-11-08 17:19:49 +0530

    Update AdminProfile.js

--- src/components/AdminProfile.js
+++ src/components/AdminProfile.js
@@ -1,16 +1,107 @@
+// import { Link, useLocation } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import AdminNav from "./AdminNav";
+
+// function AdminProfile() {
+//   const location = useLocation();
+//   const id = location.pathname.split("/").pop(); 
+
+//   const [userData, setUserData] = useState({
+//     name: "",
+//     username: "",
+//     email: "",
+//     phoneNumber: "", 
+//     address: "",
+//   });
+
+//   useEffect(() => {
+//     if (id) {
+//       fetch(`https://online-voting-app-backend.onrender.com/AdminsRoute/get-admin/${id}`)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (data) { 
+//             const user = data;
+//             setUserData({
+//               name: user.name,
+//               username: user.username,
+//               email: user.email,
+//               phoneNumber: user.phonenumber, 
+//               address: user.address,
+//             });
+//           } else {
+//             console.error("No user data found for id:", id);
+//           }
+//         })
+//         .catch((error) => {
+//           console.error("Error fetching user data:", error);
+//         });
+//     }
+//   }, [id]);
+
+//   return (
+//     <div>
+//       <AdminNav />
+//         {/* <div style={{ backgroundColor: "#FF8976",height:"30vh" }}></div> */}
+//         <div style={{backgroundColor:"rgba(255, 99, 71, 0.2)",height:"20vh"}}></div>
+//         {/* <div style={{ backgroundColor: "#ffd9b3",height:"30vh" }}></div> */}
+//       <div>
+//         <div className="text-center" style={{marginTop:"-15vh"}}>
+//               <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="admin-pic" style={{ borderRadius: "50%", height: "30vh" }} />
+//         </div>
+//       </div>
+       
+
+
+//       <div className="d-flex mt-2">
+//         <div className="col-lg-5 mx-auto">
+//               <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}}>Admin Name</p>
+//               <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
+//                 <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.name}</p>
+//               </div>
+
+//               <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">User Name</p>
+//               <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
+//                 <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.username}</p>
+//               </div>
+
+//               <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">Phone Number</p>
+//               <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
+//                 <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.phoneNumber}</p>
+//               </div>
+
+//               <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">Email</p>
+//               <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
+//                 <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.email}</p>
+//               </div>
+              
+//               <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3 ">Address</p>
+//               <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }} className="mb-5">
+//                 <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}} >{userData.address}</p>
+//               </div>
+//               <div className="text-center"><Link to={"/Admin/EditAdmin/"+id} ><button className="btn mb-5" style={{backgroundColor:"rgb(43,24,155)",color:"White"}}>Edit Data</button></Link></div>
+//           </div>
+
+          
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default AdminProfile;
+
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import AdminNav from "./AdminNav";
 
 function AdminProfile() {
   const location = useLocation();
-  const id = location.pathname.split("/").pop(); 
+  const id = location.pathname.split("/").pop();
 
   const [userData, setUserData] = useState({
     name: "",
     username: "",
     email: "",
-    phoneNumber: "", 
+    phoneNumber: "",
     address: "",
   });
 
@@ -19,13 +110,13 @@ function AdminProfile() {
       fetch(`https://online-voting-app-backend.onrender.com/AdminsRoute/get-admin/${id}`)
         .then((response) => response.json())
         .then((data) => {
-          if (data) { 
+          if (data) {
             const user = data;
             setUserData({
               name: user.name,
               username: user.username,
               email: user.email,
-              phoneNumber: user.phonenumber, 
+              phoneNumber: user.phonenumber,
               address: user.address,
             });
           } else {
@@ -41,50 +132,76 @@ function AdminProfile() {
   return (
     <div>
       <AdminNav />
-        {/* <div style={{ backgroundColor: "#FF8976",height:"30vh" }}></div> */}
-        <div style={{backgroundColor:"rgba(255, 99, 71, 0.2)",height:"20vh"}}></div>
-        {/* <div style={{ backgroundColor: "#ffd9b3",height:"30vh" }}></div> */}
+      <div style={{ backgroundColor: "#FF8976", height: "30vh" }}></div>
+      <div style={{ backgroundColor: "rgba(255, 99, 71, 0.2)", height: "20vh" }}></div>
       <div>
-        <div className="text-center" style={{marginTop:"-15vh"}}>
-              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="admin-pic" style={{ borderRadius: "50%", height: "30vh" }} />
+        <div className="text-center" style={{ marginTop: "-15vh" }}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+            alt="admin-pic"
+            style={{ borderRadius: "50%", height: "30vh" }}
+          />
         </div>
       </div>
-       
-
 
       <div className="d-flex mt-2">
         <div className="col-lg-5 mx-auto">
-              <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}}>Admin Name</p>
-              <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
-                <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.name}</p>
-              </div>
-
-              <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">User Name</p>
-              <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
-                <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.username}</p>
-              </div>
-
-              <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">Phone Number</p>
-              <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
-                <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.phoneNumber}</p>
-              </div>
-
-              <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3">Email</p>
-              <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }}>
-                <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}}>{userData.email}</p>
-              </div>
-              
-              <p style={{paddingLeft:"0.8vh",fontFamily:"serif",fontWeight:"bolder",fontSize:"3vh"}} className="mt-3 ">Address</p>
-              <div style={{ border: "1px solid #D0D0D0",borderRadius:"7px",backgroundColor:"#D0D0D0	" }} className="mb-5">
-                <p style={{padding:"1vh 0vh 0vh 1vh",fontFamily:"serif",fontSize:"2.5vh"}} >{userData.address}</p>
-              </div>
-              <div className="text-center"><Link to={"/Admin/EditAdmin/"+id} ><button className="btn mb-5" style={{backgroundColor:"rgb(43,24,155)",color:"White"}}>Edit Data</button></Link></div>
+          <p style={{ paddingLeft: "0.8vh", fontFamily: "serif", fontWeight: "bolder", fontSize: "3vh" }}>
+            Admin Name
+          </p>
+          <div style={{ border: "1px solid #D0D0D0", borderRadius: "7px", backgroundColor: "#D0D0D0" }}>
+            <p style={{ padding: "1vh 0vh 0vh 1vh", fontFamily: "serif", fontSize: "2.5vh", whiteSpace: "nowrap" }}>
+              {userData.name}
+            </p>
           </div>
 
-          
+          <p style={{ paddingLeft: "0.8vh", fontFamily: "serif", fontWeight: "bolder", fontSize: "3vh" }} className="mt-3">
+            User Name
+          </p>
+          <div style={{ border: "1px solid #D0D0D0", borderRadius: "7px", backgroundColor: "#D0D0D0" }}>
+            <p style={{ padding: "1vh 0vh 0vh 1vh", fontFamily: "serif", fontSize: "2.5vh", whiteSpace: "nowrap" }}>
+              {userData.username}
+            </p>
+          </div>
+
+          <p style={{ paddingLeft: "0.8vh", fontFamily: "serif", fontWeight: "bolder", fontSize: "3vh" }} className="mt-3">
+            Phone Number
+          </p>
+          <div style={{ border: "1px solid #D0D0D0", borderRadius: "7px", backgroundColor: "#D0D0D0" }}>
+            <p style={{ padding: "1vh 0vh 0vh 1vh", fontFamily: "serif", fontSize: "2.5vh", whiteSpace: "nowrap" }}>
+              {userData.phoneNumber}
+            </p>
+          </div>
+
+          <p style={{ paddingLeft: "0.8vh", fontFamily: "serif", fontWeight: "bolder", fontSize: "3vh" }} className="mt-3">
+            Email
+          </p>
+          <div style={{ border: "1px solid #D0D0D0", borderRadius: "7px", backgroundColor: "#D0D0D0" }}>
+            <p style={{ padding: "1vh 0vh 0vh 1vh", fontFamily: "serif", fontSize: "2.5vh", whiteSpace: "nowrap" }}>
+              {userData.email}
+            </p>
+          </div>
+
+          <p style={{ paddingLeft: "0.8vh", fontFamily: "serif", fontWeight: "bolder", fontSize: "3vh" }} className="mt-3 ">
+            Address
+          </p>
+          <div style={{ border: "1px solid #D0D0D0", borderRadius: "7px", backgroundColor: "#D0D0D0" }} className="mb-5">
+            <p style={{ padding: "1vh 0vh 0vh 1vh", fontFamily: "serif", fontSize: "2.5vh", whiteSpace: "nowrap" }}>
+              {userData.address}
+            </p>
+          </div>
+          <div className="text-center">
+            <Link to={"/Admin/EditAdmin/" + id}>
+              <button className="btn mb-5" style={{ backgroundColor: "rgb(43,24,155)", color: "White" }}>
+                Edit Data
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default AdminProfile;
+
